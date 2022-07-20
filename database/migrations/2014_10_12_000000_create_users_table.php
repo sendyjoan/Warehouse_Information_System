ac4d0c0a519681dbe952b3b1fef3e20c17ea9a56
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-07-21 05:55:23 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id('NIP');
             $table->string('name');
             $table->string('email')->unique();
+            $table->text('alamat');
+            $table->string('nomor_telephone', 15);
+            $table->string('isBeku', 2);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
