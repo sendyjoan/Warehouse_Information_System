6a6737fc922b3906ee3b8bffd60a41f93fd653ea
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-07-21 06:06:33 +0700

    Add Migration Supplier

---
+++ database/migrations/2022_07_20_230200_create_suppliers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id('NIS');
+            $table->string('nama_lengkap');
+            $table->string('alamat');
+            $table->date('tanggal_lahir');
+            $table->string('nomor_telephone', 15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+};
