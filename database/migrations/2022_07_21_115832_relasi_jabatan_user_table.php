e06d26ddd2e56cb9ea0cb384cefb16a5c31239b5
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-07-21 19:07:14 +0700

    Create Relation Table Users, Jabatan, and Genders

---
+++ database/migrations/2022_07_21_115832_relasi_jabatan_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table){
+            $table->unsignedBigInteger('jabatan_id')->nullable();
+            $table->foreign('jabatan_id')->references('id')->on('jabatan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table){
+            $table->dropForeign(['jabatan_id']);
+            $table->dropColumn('jabatan_id');
+        });
+    }
+};
