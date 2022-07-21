1652109866bdb63913394a9328c93a39c9f0f857
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-07-21 11:58:18 +0700

    Update 2022_07_21_045337_create_barangkeluar_table.php

--- database/migrations/2022_07_21_045337_create_barangkeluar_table.php
+++ database/migrations/2022_07_21_045337_create_barangkeluar_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('barangkeluar', function (Blueprint $table) {
             $table->id();
+            $table->string('dokumen_pendukung');
             $table->timestamps();
         });
     }
