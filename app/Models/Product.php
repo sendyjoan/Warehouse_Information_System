15626896934c282521e2cb015a54d2d1798dd18b
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-07-22 03:34:58 +0700

    Create Model Relation Satuan and Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Satuan;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
-    use HasFactory;
+    protected $table = 'products';
+    protected $primaryKey = 'barcode';
+
+    protected $fillable = [
+        'barcode',
+        'nama_product',
+        'perusahaan_NIPS',
+        'satuan_id',
+    ];
+
+    public function satuan(){
+        return $this->belongsTo(Satuan::class);
+    }
 }
--- app/Models/Satuan.php
+++ app/Models/Satuan.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Satuan extends Model
 {
     use HasFactory;
+    protected $table='satuans';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'id',
+        'nama_satuan',
+    ];
+
+    public function product(){
+        return $this->hasMany(Product::class);
+    }
 }
