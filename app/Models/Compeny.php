748518c2babefa8b06e2d3122238eef35636f1c3
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-07-22 03:56:18 +0700

    Create Model Relation Compeny and Verified

--- app/Models/Compeny.php
+++ app/Models/Compeny.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Verified;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Compeny extends Model
 {
     use HasFactory;
+    protected $table = 'compenies';
+    protected $primaryKey = 'NIPS';
+
+    protected $fillable = [
+        'NIPS',
+        'nama_perusahaan',
+        'alamat_perusahaan',
+        'verified_id',
+    ];
+
+    public function verified(){
+        return $this->belongsTo(Verified::class);
+    }
 }
--- app/Models/Verified.php
+++ app/Models/Verified.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Compeny;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Verified extends Model
 {
     use HasFactory;
+    protected $table = 'verifieds';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'id',
+        'nama_verified',
+    ];
+
+    public function compeny(){
+        return $this->hasMany(Compeny::class);
+    }
 }
