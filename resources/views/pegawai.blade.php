5d1760f400a4ebdcd1025c68f134af1f2a61ee09
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-07-29 16:40:12 +0700

    Update pegawai.blade.php

--- resources/views/pegawai.blade.php
+++ resources/views/pegawai.blade.php
@@ -24,6 +24,13 @@
                     </div>
                 <!-- /.card-header -->
                     <div class="card-body">
+                        <div class="float-right">
+                            <a class="btn btn-primary btn-sm" data-toggle="modal" data-target="#createPegawai">
+                                <i class="fas fa-pencil-alt">
+                                </i>
+                                Tambah Pegawai
+                            </a>
+                        </div>
                     <table id="example2" class="table table-bordered table-hover">
                         <thead>
                         <tr class="text-center">
@@ -49,17 +56,17 @@
                             <span class="badge badge-success">Success</span>
                         </td>
                         <td class="project-actions">
-                            <a class="btn btn-primary btn-sm" href="#">
+                            <a class="btn btn-info btn-sm" data-toggle="modal" data-target="#showPegawai">
                                 <i class="fas fa-folder">
                                 </i>
                                 View
                             </a>
-                            <a class="btn btn-info btn-sm" href="#">
+                            <a class="btn btn-warning btn-sm" href="#">
                                 <i class="fas fa-pencil-alt">
                                 </i>
                                 Edit
                             </a>
-                            <a class="btn btn-info btn-sm" href="#">
+                            <a class="btn btn-dark btn-sm" href="#">
                                 <i class="fas fa-pencil-alt">
                                 </i>
                                 Edit Status
@@ -73,6 +80,77 @@
                         </tr>
                         </tbody>
                     </table>
+                    </div>
+                    <!-- Modal Tambah Pegawai-->
+                    <div class="modal fade" id="createPegawai" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">Tambah Pegawai</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            </div>
+                            <div class="modal-body">
+                            <form action="" method="post">
+                                <div class="form-group">
+                                    <label for="NamaLengkap">Nama Lengkap</label>
+                                    <input type="text" class="form-control" id="namalengkap" name="name" placeholder="Nama Pegawai">
+                                </div>
+                                <div class="form-group">
+                                    <label for="Email">Email</label>
+                                    <input type="email" class="form-control" id="email" name="email" placeholder="EmailPegawai@email.com">
+                                </div>
+                                <div class="form-group">
+                                    <label for="Alamat">Alamat</label>
+                                    <input type="text" class="form-control" id="alamat" name="alamat" placeholder="Alamat Pegawai">
+                                </div>
+                                <div class="form-group">
+                                    <label for="No Telephone">Nomor Telephone</label>
+                                    <input type="text" class="form-control" id="nomor_telephone" name="nomor_telephone" placeholder="Nomor Telephone Pegawai">
+                                </div>
+                                <div class="form-group">
+                                    <label for="Tanggal Lahir">Tanggal Lahir</label>
+                                    <input type="date" class="form-control" id="tanggal_lahir" name="tanggal_lahir" placeholder="Tanggal Lahir Pegawai">
+                                </div>
+                                <div class="form-group">
+                                    <label for="Password">Password</label>
+                                    <input type="text" class="form-control" id="password" name="password" placeholder="Masukkan Password">
+                                    <input type="text" class="form-control" id="repassword" name="repassword" placeholder="Masukkan Ulang Password">
+                                </div>
+                                <div class="form-group float-right">
+                                    <button type="submit" class="btn btn-primary">Simpan Pegawai</button>
+                                </div>
+                            </form>
+                            </div>
+                            <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+
+                    {{-- Modal Show Pegawai --}}
+                    <div class="modal fade" id="showPegawai" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <h1 class="text-center">Sendy Joan Kevin</h1>
+                                    <p>Nomor Induk Pegawai : </p>
+                                    <p></p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 <!-- /.card-body -->
                 </div>
